86edb68137f0ed2edbd9205c017c0b48db291ac7
Abdullah <heighter79@gmail.com>
2023-11-27 11:12:31 +0500

    day 2

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ class Bot:
         # self.chrome_options = webdriver.ChromeOptions()
         # self.chrome_options.add_experimental_option("detach", True)
         # self.driver = webdriver.Chrome(options=self.chrome_options)
-        self.web_url = "https://www.zameen.com/Rentals/Karachi-2-1.html?price_max=50000"
+        self.max_rent = 50000
+        self.web_url = f"https://www.zameen.com/Rentals/Karachi-2-1.html?price_max={self.max_rent}"
         self.web_data = requests.get(url=self.web_url)
         self.web_data_text_format = self.web_data.text
         self.soup = BeautifulSoup(self.web_data_text_format, "html.parser")
@@ -25,9 +26,16 @@ class Bot:
             price_list.append(prices.text)
 
     def scrap_property_links(self):
-        link = self.soup.findAll("a")
-        for links in link:
-            print(links.text)
+        elements_with_class = self.soup.find_all(class_="_357a9937")
+
+        # Iterate through each element and find <a> elements within it
+        for element in elements_with_class:
+            links = element.find_all("a")
+            for link in links:
+                link_list.append(link['href'])
+
+        return link_list
+
 
 
 d = Bot()
@@ -35,3 +43,5 @@ d.scrap_property_prices()
 d.scrap_property_links()
 
 print(price_list)
+print(link_list)
+
